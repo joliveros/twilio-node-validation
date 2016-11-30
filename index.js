a58aae41bd4d43d5ba7915ce81ffd8a6d2ce6b5a
José Oliveros <jose.oliveros.1983@gmail.com>
2016-11-30 15:43:15 -0600

    Remove custom middleware and use the built in twilio webhook() middleware

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-var twilio = require('twilio');
 // You can find your Twilio Auth Token here: https://www.twilio.com/console
 // Which can be set at runtime as follows:
 // $ TWILIO_AUTH_TOKEN=XXXXXXXXXXXXXXXXXXX node index.js
-var TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
-
-function validateTwilioRequest() {
-  var routes = Array.prototype.slice.call(arguments);
-
-  return function(req, res, next) {
-    // disable this middleware when testing
-    if (process.env.NODE_ENV == 'test') {
-      return next();
-    }
-
-    var shouldValidateRequest = false;
-
-    routes.forEach(function(route) {
-      // Ensure this middleware is initialized properly:
-      // i.e. app.use(validateTwilioRequest('/voice', '/message'));
-      if (typeof route !== 'string') {
-        throw new Error('This middleware only accepts urls as strings.');
-      }
-
-      if (shouldValidateRequest) {
-        return;
-      } else {
-        shouldValidateRequest = req.url.indexOf(route) > -1;
-      }
-    });
-
-    if (shouldValidateRequest) {
-      if (twilio.validateExpressRequest(req, TWILIO_AUTH_TOKEN)) {
-        next();
-      } else {
-        res
-        .type('text/plain')
-        .status(403)
-        .send('You are not Twilio >:/');
-      }
-    } else {
-      next();
-    }
-  };
-}
-
+var twilio = require('twilio');
 var app = require('express')();
 var bodyParser = require('body-parser');
-// var twilio = require('twilio');
 var TwimlResponse = twilio.TwimlResponse;
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(validateTwilioRequest('/voice', '/message'));
-
-app.post('/voice', function(req, res) {
+app.post('/voice', twilio.webhook(), function(req, res) {
   // Twilio Voice URL - receives incoming calls from Twilio
   var response = new TwimlResponse();
 
@@ -66,7 +21,7 @@ app.post('/voice', function(req, res) {
   res.send(response.toString());
 });
 
-app.post('/message', function(req, res) {
+app.post('/message', twilio.webhook(), function(req, res) {
   // Twilio Messaging URL - receives incoming messages from Twilio
   var response = new TwimlResponse();
 
